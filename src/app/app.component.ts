8ffd7bd4f6940c67ce12ab7f3ff4e6928f4c74dd
Evandro <vando86206@gmail.com>
2026-05-07 21:30:18 -0300

    app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { RegisterComponent } from './pages/register/register.component';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet, RegisterComponent],
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent {
+//   title = 'front_agendamento_oficina';
+// }
 import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { RegisterComponent } from './pages/register/register.component';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'front_agendamento_oficina';
-}
+export class AppComponent {}
+
+src / main.ts(bootstrapping);
+
+typescript;
+
+bootstrapApplication(AppComponent, appConfig);
