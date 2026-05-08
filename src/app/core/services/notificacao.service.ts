e698cf0d20e0dba2fd718c39be6911267b5a5daf
Evandro <vando86206@gmail.com>
2026-05-07 21:10:53 -0300

    NotificacaoService

--- src/app/core/services/notificacao.service.ts
+++ src/app/core/services/notificacao.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, interval, switchMap } from 'rxjs';
+import { Notificacao } from '../../models/notificacao';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class NotificacaoService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8000/api/v1/notificacoes';
+  private audio = new Audio('/assets/alert.mp3');
 
-  constructor() { }
+  listarNaoLidas(): Observable<Notificacao[]> {
+    return this.http.get<Notificacao[]>(this.apiUrl);
+  }
+
+  marcarLida(id: string): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}/ler`, {});
+  }
+
+  // Inicia polling a cada 30s
+  iniciarPolling(callback: (notif: Notificacao) => void) {
+    interval(30000)
+      .pipe(switchMap(() => this.listarNaoLidas()))
+      .subscribe((notificacoes) => {
+        notificacoes.forEach((notif) => {
+          if (!notif.lida) {
+            callback(notif);
+            this.tocarSom();
+            this.marcarLida(notif.id).subscribe();
+          }
+        });
+      });
+  }
+
+  tocarSom() {
+    this.audio
+      .play()
+      .catch((e) => console.log('Áudio bloqueado pelo navegador'));
+  }
+
+  exibirPopup(notif: Notificacao) {
+    // Usa window.alert para simplicidade, mas poderia ser modal customizado
+    alert(`${notif.titulo}\n${notif.mensagem}`);
+  }
 }
