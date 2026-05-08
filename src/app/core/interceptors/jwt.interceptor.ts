c127582b862a88940b31e6b744398a0b0c84e108
Evandro <vando86206@gmail.com>
2026-05-07 21:12:34 -0300

    JwtInterceptor

---
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  private auth = inject(AuthService);
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const token = this.auth.getToken();
+    if (token) {
+      req = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+      });
+    }
+    return next.handle(req);
+  }
+}
