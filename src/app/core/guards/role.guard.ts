bd6415c390b8d5ad14a2bba76930be24a3e92a74
Evandro-ESD <vando86206@gmail.com>
2026-05-09 12:42:45 -0300

    Update role.guard.ts login fake

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,4 +1,4 @@
-import { inject } from '@angular/core';
+/*import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserRole } from '../../models/user';
 import { AuthService } from '../services/auth.service';
@@ -10,5 +10,38 @@ export const roleGuard = (allowedRoles: UserRole[]) => {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     if (allowedRoles.includes(user.role)) return true;
     return router.parseUrl('/comando'); // redireciona para área principal (sem acesso)
+  };
+};
+*/
+
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserRole } from '../../models/user';
+import { AuthService } from '../services/auth.service';
+
+export const roleGuard = (
+  allowedRoles: UserRole[]
+) => {
+
+  return () => {
+
+    const auth = inject(AuthService);
+
+    const router = inject(Router);
+
+    const user = auth.currentUser();
+
+    if (!user) {
+      return router.parseUrl('/login');
+    }
+
+    if (
+      allowedRoles.includes(user.role)
+    ) {
+      return true;
+    }
+
+    return router.parseUrl('/comando');
   };
 };
