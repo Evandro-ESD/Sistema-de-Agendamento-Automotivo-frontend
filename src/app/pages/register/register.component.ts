208a983b9cb811602dd4aa29386f5a1fdecc4667
Evandro <vando86206@gmail.com>
2026-05-08 23:55:32 -0300

    register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,68 +1,70 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-register',
-//   imports: [],
-//   templateUrl: './register.component.html',
-//   styleUrl: './register.component.css'
-// })
-// export class RegisterComponent {
-
-// }
 import { CommonModule } from '@angular/common';
+
 import { Component, inject } from '@angular/core';
+
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+
+import { Router, RouterLink } from '@angular/router';
+
 import { AuthService } from '../../core/services/auth.service';
+
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+import { HeroComponent } from '../../shared/components/hero/hero.component';
+
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
-  template: `
-    <div class="container" style="max-width: 400px;">
-      <h2>CADASTRO (ASSOCIADO)</h2>
-      <hr />
-      <form [formGroup]="form" (ngSubmit)="onSubmit()">
-        <div><label>Nome</label><input formControlName="nome" /></div>
-        <div>
-          <label>Email</label><input type="email" formControlName="email" />
-        </div>
-        <div>
-          <label>Senha</label
-          ><input type="password" formControlName="password" />
-        </div>
-        <div *ngIf="errorMsg" class="error">{{ errorMsg }}</div>
-        <button type="submit">REGISTRAR</button>
-      </form>
-    </div>
-  `,
+
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+  ],
+
+  templateUrl: './register.component.html',
 })
 export class RegisterComponent {
   private fb = inject(FormBuilder);
+
   private auth = inject(AuthService);
+
   private router = inject(Router);
+
   errorMsg = '';
 
   form = this.fb.group({
     nome: ['', Validators.required],
+
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.minLength(6)],
+
+    password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
   onSubmit() {
-    if (this.form.valid) {
-      this.auth
-        .register(
-          this.form.value.nome!,
-          this.form.value.email!,
-          this.form.value.password!,
-        )
-        .subscribe({
-          next: () => this.router.navigate(['/login']),
-          error: (err) =>
-            (this.errorMsg = err.error?.detail || 'Erro no cadastro'),
-        });
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
     }
+
+    this.auth
+      .register(
+        this.form.value.nome!,
+        this.form.value.email!,
+        this.form.value.password!,
+      )
+      .subscribe({
+        next: () => this.router.navigate(['/login']),
+
+        error: (err) => {
+          this.errorMsg = err.error?.detail || 'Erro no cadastro';
+        },
+      });
   }
 }
