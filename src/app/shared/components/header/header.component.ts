e49efaed751d0ad084953a82b1e003a009a06814
Evandro <vando86206@gmail.com>
2026-05-12 09:01:07 -0300

    links no botao hamburguer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [],
 })
 export class HeaderComponent implements OnInit {
+  menuOpen = false;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
 }
