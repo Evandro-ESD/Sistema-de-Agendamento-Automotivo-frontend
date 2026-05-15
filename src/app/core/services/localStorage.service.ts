9a8355082e9ec5cb2c92436135b7b9c4f7c1beb7
Evandro <vando86206@gmail.com>
2026-05-15 15:24:54 -0300

    atualizado localstorageservice

--- src/app/core/services/localStorage.service.ts
+++ src/app/core/services/localStorage.service.ts
@@ -6,11 +6,36 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
   constructor() {}
 
-  get<T>(key: string): T[] {
+  get<T>(key: string): T | null {
     const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : [];
+    if (!data) {
+      return null;
+    }
+    try {
+      return JSON.parse(data) as T;
+    } catch (error) {
+      console.log(`Erro ao fazer parse da chave ${key}`, error);
+      return null;
+    }
   }
-  set<T>(key: string, value: T[]): void {
+  set<T>(key: string, value: T): void {
     localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  remove(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+
+  update<T>(key: string, callback: (data: T) => T): void {
+    const currentData = this.get<T>(key);
+
+    if (currentData) {
+      const updated = callback(currentData);
+      this.set(key, updated);
+    }
   }
 }
